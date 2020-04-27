71705bed9c4142788e03db95b21151621ddfe4f1
Tshiamo <tshiamobhuda@gmail.com>
2020-04-27 05:22:58 +0200

    Added click events for all menu items - Added menu separators

--- indicator.py
+++ indicator.py
@@ -26,15 +26,36 @@ class Indicator():
         self.logs_action = QAction('Logs')
         self.exit_action = QAction('Exit')
 
+        self.connect_action.triggered.connect(self._click_connect)
+        self.connect_action.triggered.connect(self._click_disconnect)
+        self.connect_action.triggered.connect(self._click_config)
+        self.connect_action.triggered.connect(self._click_logs)
         self.exit_action.triggered.connect(self.app.quit)
 
         menu.addAction(self.connect_action)
         menu.addAction(self.disconnect_action)
+        menu.addSeparator()
         menu.addAction(self.config_action)
         menu.addAction(self.logs_action)
+        menu.addSeparator()
         menu.addAction(self.exit_action)
 
         return menu
+
+    def _click_connect(self):
+        pass
+
+    def _click_disconnect(self):
+        pass
+
+    def _click_config(self):
+        pass
+
+    def _click_logs(self):
+        pass
+
+    def _click_exit(self):
+        pass
 
 
 if __name__ == '__main__':
